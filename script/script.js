21d64ceefc33ce660f4f1e40eda93840697860f3
10994015 <a0938599191@gmail.com>
2021-09-23 22:15:31 +0800

    首頁完成

---
+++ script/script.js
@@ -0,0 +1,76 @@
+var lightControlBigbtn = document.getElementsByClassName('lightControlBigbtn')[0];
+var lightControlbtn = document.getElementsByClassName('lightControlbtn');
+var off = document.getElementById("off");
+var on = document.getElementById("on");
+
+on.classList.add("onLightColor");
+
+on.addEventListener("click",()=>{
+    on.classList.add("onLightColor");
+    on.classList.remove("offLightColor");
+    off.classList.remove("onLightColor");
+    off.classList.add("offLightColor");
+});
+off.addEventListener("click",()=>{
+    off.classList.add("onLightColor");
+    off.classList.remove("offLightColor");
+    on.classList.remove("onLightColor");
+    on.classList.add("offLightColor");
+});
+
+for(var i=0;i<lightControlbtn.length;i++){
+    lightControlbtn[i].classList.add("onLightColor");
+    lightControlbtn[i].addEventListener("click",lightClick)
+}
+lightControlBigbtn.classList.add("onLightColor");
+
+lightControlBigbtn.addEventListener("click",()=>{
+    big = lightControlBigbtn.className.length
+    if(big==32){
+        lightControlBigbtn.classList.remove("offLightColor");
+        lightControlBigbtn.classList.add("onLightColor");
+        for(var x=0;x<lightControlbtn.length;x++){
+            lightControlbtn[x].classList.add("onLightColor");
+            lightControlbtn[x].classList.remove("offLightColor");
+        }
+    }else{
+        lightControlBigbtn.classList.remove("onLightColor");
+        lightControlBigbtn.classList.add("offLightColor");
+        for(var x=0;x<lightControlbtn.length;x++){
+            lightControlbtn[x].classList.remove("onLightColor");
+            lightControlbtn[x].classList.add("offLightColor");
+        }
+    }
+})
+function lightClick(){
+    
+  if(this.className.length==29){
+    this.classList.remove("offLightColor");
+    this.classList.add("onLightColor");
+  }else{
+    this.classList.remove("onLightColor");
+    this.classList.add("offLightColor");
+  }
+
+  
+    if(lightControlbtn[0].className.length==28 & 
+        lightControlbtn[1].className.length==28 &
+        lightControlbtn[2].className.length==28 &
+        lightControlbtn[3].className.length==28
+    ){
+            lightControlBigbtn.classList.remove("offLightColor");
+            lightControlBigbtn.classList.add("onLightColor");
+    }else{
+        lightControlBigbtn.classList.add("offLightColor");
+            lightControlBigbtn.classList.remove("onLightColor");
+    }
+    
+    
+    
+}
+
+function clearLight(){
+    for(var n=0;n<lightControlbtn.length;n++){
+        lightControlbtn[n].style.background = "#EFEFEF";
+    }
+}
